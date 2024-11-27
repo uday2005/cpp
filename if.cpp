974f3118bbdb9da4a5d90ab0452261a7a6db656d
Uday <24b2729@iitb.ac.in>
2024-11-27 20:11:17 +0530

    prime1

--- if.cpp
+++ if.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-
-int main()
-{   
-    int x{};
-    std::cout << "enter an integer"  << "\n";
-    std::cin >> x;
-
-    if (x == 0)
-    std::cout << "both are euqal"  << "\n";
-    else
-    std::cout << "both are not equal" << "\n";
-
-    return 0;
-//if statement execuets only single condition
-
+ int main()
+{
+int x{};
+std::cout << "input an integer between 0 and 9" << "\n";
+std::cin >> x;
+if (x==1)
+std::cout << "The number is not prime";
+else if (x > 3  && (x%2  == 0 || x%3 == 0))
+std::cout << "The number is not prime";
+else
+std::cout << "The number is prime";
 }
-
