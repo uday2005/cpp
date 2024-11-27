ec8f60cad02960561622be3f3caa6d4363e79c56
Uday <24b2729@iitb.ac.in>
2024-11-27 20:29:53 +0530

    prime2

--- if.cpp
+++ if.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+
+bool isprime(int x)
+{
+    if (x == 2)
+        return true;
+    else if (x == 3)
+        return true;
+    else if (x == 5)
+        return true;
+    else if (x == 7)
+        return true;
+    
+    return false;
+}
  int main()
 {
-int x{};
-std::cout << "input an integer between 0 and 9" << "\n";
-std::cin >> x;
-if (x==1)
-std::cout << "The number is not prime";
-else if (x > 3  && (x%2  == 0 || x%3 == 0))
-std::cout << "The number is not prime";
-else
-std::cout << "The number is prime";
+    std::cout << "input the integer between 0 to 9" << "\n";
+    int x {};
+    std::cin >> x;
+
+    if (isprime(x))
+    std::cout << "This is a prime number" << "\n";
+    else
+    std::cout << "this is not a prime number" << "\n";
 }
+
+// this solution will be only valid if the number will be very small 
+// as in the question only we have 0 to 9
