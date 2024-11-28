2f034d3b984146810049119cd3e835fe3be78ab4
Uday <24b2729@iitb.ac.in>
2024-11-29 00:56:43 +0530

    learncpp.cpp

--- learncpp.cpp
+++ learncpp.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <string>  // allows the use of the std::string and std::getline
-using namespace std;
+#include <string>  // for std::string
+#include <string_view>  //for std::string_view
+//int x { 5 }; // x makes a copy of its initializer
+//std::string s{ "Hello, world!" }; // s makes a copy of its initializer
+
+//when s is initialized, the C-style string literal "Hello, world!" is 
+//copied into memory allocated for std::string s. Unlike fundamental types, initializing and copying a std::string is slow
+
+//all we do with s is print the value to the console, 
+//and then s is destroyed. We’ve essentially made a copy of “Hello, world!” 
+//just to print and then destroy that copy
+
+//Prefer std::string_view over std::string when you need a read-only string, especially for function parameters.
 
 int main ()
 {
+// THE Good thing about std::string_view is it can be intialsed with c styled string
+// or with std::string or with aouther std::string_view
+    std::string_view s1 {"Hello World"}; // intilalsed with c style string 
+    std::cout << s1 <<'\n';
+
+    std::string s{"Hello world"};
+    std::string_view s2 {s};
+    std::cout << s2 <<'\n';
+
+    std::string_view s3 {s2};
+    std::cout << s3 << '\n';
 
 
-    std::cout << "what is your name" << '\n';
-    std::string name {};
-    //std::cin >> name;
-    std::getline(std::cin >> std::ws,name);  // read a full line of text into name
-    // so when we use th std::cin it just reads name and stop after encounting the whitespace
-    //std::ws do that it gives std::cin to ignore the take the th ewhitespace in input 
+    using namespace std::string_literals;      // access the s suffix
+    using namespace std::string_view_literals; // access the sv suffix
 
-    // std::cout << "what is your age" <<'\n';
-    // std::string age{};
-    // //std::cin >> color;
-    // std::getline(std::cin >> std::ws, age);
-    int age = 25;
+    std::cout << "foo\n";   // no suffix is a C-style string literal
+    std::cout << "goo\n"s;  // s suffix is a std::string literal
+    std::cout << "moo\n"sv; // sv suffix is a std::string_view literal
 
-
-    int letter = static_cast<int>(name.length());
-// so here the name.length() gives unsigned values and age is giving signed values so we can do add 
-//operation directly to them 
-    std::cout << "Your age " <<" + " << "length of name is" << letter + age << '\n';
-
-//so the thing is we input our name and it will of only one letter without whitespace
-//then this will run fine and ask next what is your favourit ecolor and proceed to do so
-
-//but the problem is when we have a white spaces between the like uday kumar so the cin will break
-//at the first whitespace it will encounter and waiting fro anouther cin and give color the value 
-//of last name so it will not take input of color
-
-
-// so to take the input as on eline we may use the inbulit function 
-//std::getline which will tak etwo thing as input
-
-// to know length of std::string function we use 
-    std::cout << "The length of name " << name.length() <<'\n';
-
-//Although std::string is required to be null-terminated 
-//(as of C++11), the returned length of a std::string does not include the implicit null-terminator character
-//Do not pass std::string by value, as it makes an expensive copy
+//Unlike std::string, std::string_view has full support for constexpr:
+//This makes constexpr std::string_view the preferred choice when string symbolic constants are needed.
     return 0;
 }
