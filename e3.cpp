b23286bc0312252f79fae304616e4b019273bef7
Uday <24b2729@iitb.ac.in>
2024-11-28 12:25:44 +0530

    write with more function

--- e3.cpp
+++ e3.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-int GetHeight()
+double GetHeight()
 {
-    int x {};
+    int TowerHeight {};
     std::cout << "enter the height of tower in meters : ";
-    std::cin >> x;
-    return x;
+    std::cin >> TowerHeight;
+    return TowerHeight;
 }
-double HeightTravelled(int time)
+double HeightTravelled(double TowerHeight ,int time)
 {
-    double height {};
-    height = (9.8 * (time*time))/2;
-    return height;
+    double Falldistance {(9.8 * time *time)/2};
+    double Ballheight {(TowerHeight-Falldistance)};
+    return Ballheight;
     
+}
+void PrintBallHeight (double Ballheight , int time)
+{
+    if (Ballheight > 0.0)
+        std::cout << "At " << time  << " seconds , the ball is at height " <<Ballheight <<"meterts\n";
+    else
+        std::cout << "At" << time << "ball is at ground \n";
 }
 int main()
 {
-    int time{};
-    std::cout << "time at which you want the height" << '\n';
-    std::cin >> time;
-    double height1 {GetHeight()};
-    double height2 {HeightTravelled(time)};
-    double height{height1-height2};
-    if (height >0)
-    std::cout << "At " << time << "seconds, the ball is at height:" << height;
-    else
-    std::cout << "Ball is on the ground";
+int time {2};
+double TowerHeight {GetHeight()};
+double Ballheight {HeightTravelled(TowerHeight,time)};
+PrintBallHeight (Ballheight,time);
 
 
 }
