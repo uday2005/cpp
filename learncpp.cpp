9842f50bec38d4722b88a8fd7c8d3a38931373c8
Uday <24b2729@iitb.ac.in>
2024-11-28 20:00:31 +0530

    cin issue when whitespaces is encounted

--- learncpp.cpp
+++ learncpp.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main ()
 {
-    std::string name{"Uday"};  //intilaised string name to uday
-    name = "uddesh" ; // chnage the value of name to uddesh 
 
-// std::string can be ouptput by using std::cout 
-    std::cout << "my name is " << name << '\n';
+
+    std::cout << "what is your name" << '\n';
+    std::string name {};
+    std::cin >> name;
+
+    std::cout << "what is your favourite color" <<'\n';
+    std::string color{};
+    std::cin >> color;
+
+    std::cout << "Your name is " << name<< " and your favourite color is " << color <<'\n';
+
+//so the thing is we input our name and it will of only one letter without whitespace
+//then this will run fine and ask next what is your favourit ecolor and proceed to do so
+
+//but the problem is when we have a white spaces between the like uday kumar so the cin will break
+//at the first whitespace it will encounter and waiting fro anouther cin and give color the value 
+//of last name so it will not take input of color
     return 0;
 }
