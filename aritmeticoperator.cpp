4031fb961ec19a8a46f3bd1583c64b4b1f724316
Uday <24b2729@iitb.ac.in>
2024-11-30 18:02:01 +0530

    even odd

--- aritmeticoperator.cpp
+++ aritmeticoperator.cpp
@@ -1,42 +1,24 @@
-#include <cmath>
-
-double x{ std::pow(3.0, 4.0) }; // 3 to the 4th power
-//we dont have any arithmetic e expression for calculationg arithmetic exponent
-
-// we can explicitly define our own power function
-
-//the result of pow not be good due to input of double
-//and that  is beacusing of rounding off
-
-#include <cassert> // for assert
-#include <cstdint> // for std::int64_t
 #include <iostream>
 
-// note: exp must be non-negative
-// note: does not perform range/overflow checking, use with caution
-constexpr std::int64_t powint(std::int64_t base, int exp)
+constexpr bool isEven (int num)
 {
-	assert(exp >= 0 && "powint: exp parameter has negative value");
-
-	// Handle 0 case
-	if (base == 0)
-		return (exp == 0) ? 1 : 0;
-
-	std::int64_t result{ 1 };
-	while (exp > 0)
-	{
-		if (exp & 1)  // if exp is odd
-			result *= base;
-		exp /= 2;
-		base *= base;
-	}
-
-	return result;
+    if (num % 2 == 0)
+        return true;
+    else
+        return false ;
 }
 
-int main()
-{
-	std::cout << powint(7, 12) << '\n'; // 7 to the 12th power
 
-	return 0;
+int main ()
+{
+    std::cout << "Enter a natural number" <<'\n';
+    int num {};
+    std::cin >> num ;
+
+    if (isEven(num))
+        std::cout << num << "is a even number"  << '\n';
+    else
+        std::cout << num << "is a odd number" << '\n';
+
+
 }
